82248a1247296ababbc3bfda2fc762c032da2b9a
Chung Anh <chunganhbk@gmail.com>
2019-03-15 19:17:17 +0700

    change forder frontend

---
+++ web/src/utils/constants.js
@@ -0,0 +1,10 @@
+const name =  'File manger'
+const logoURL = require('../assets/logo.png')
+const loginPage = "login"
+const baseURL = process.env.VUE_APP_BASE_URL
+export {
+  name,
+  logoURL,
+    baseURL,
+  loginPage
+}
